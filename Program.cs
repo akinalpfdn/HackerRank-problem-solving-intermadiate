c0f4bcfd1b5784d4118ad90abd345edc46a38e0d
akinalpfdn <81157056+akinalpfdn@users.noreply.github.com>
2023-06-14 12:06:49 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,129 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+
+
+class Result
+{
+
+    /*
+     * Complete the 'numberOfWays' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts 2D_INTEGER_ARRAY roads as parameter.
+     */
+
+    public static int numberOfWays(List<List<int>> roads)
+    {
+        int cityCount = roads.Count + 1;
+        int totalCount = 0;
+
+        // Iterate over all possible combinations of three cities
+        for (int city1 = 1; city1 <= cityCount; city1++)
+        {
+            for (int city2 = city1 + 1; city2 <= cityCount; city2++)
+            {
+                for (int city3 = city2 + 1; city3 <= cityCount; city3++)
+                {
+                    // Check if the distance between every pair of hotels is equal
+                    if (IsDistanceEqual(city1, city2, city3, roads))
+                    {
+                        totalCount++;
+                    }
+                }
+            }
+        }
+
+        return totalCount;
+    }
+
+    public static bool IsDistanceEqual(int city1, int city2, int city3, List<List<int>> roads)
+    {
+        int distance12 = CalculateDistance(city1, city2, roads);
+        int distance13 = CalculateDistance(city1, city3, roads);
+        int distance23 = CalculateDistance(city2, city3, roads);
+
+        // Check if the distances between every pair of cities are equal
+        return distance12 == distance13 && distance13 == distance23;
+    }
+
+    public static int CalculateDistance(int city1, int city2, List<List<int>> roads)
+    {
+        // Breadth-First Search to calculate the minimum number of roads to cross
+        Queue<int> queue = new Queue<int>();
+        HashSet<int> visited = new HashSet<int>();
+        Dictionary<int, int> distance = new Dictionary<int, int>();
+
+        queue.Enqueue(city1);
+        visited.Add(city1);
+        distance[city1] = 0;
+
+        while (queue.Count > 0)
+        {
+            int currentCity = queue.Dequeue();
+
+            if (currentCity == city2)
+            {
+                return distance[currentCity];
+            }
+
+            foreach (List<int> road in roads)
+            {
+                int startCity = road[0];
+                int endCity = road[1];
+
+                if (startCity == currentCity && !visited.Contains(endCity))
+                {
+                    queue.Enqueue(endCity);
+                    visited.Add(endCity);
+                    distance[endCity] = distance[currentCity] + 1;
+                }
+                else if (endCity == currentCity && !visited.Contains(startCity))
+                {
+                    queue.Enqueue(startCity);
+                    visited.Add(startCity);
+                    distance[startCity] = distance[currentCity] + 1;
+                }
+            }
+        }
+
+        // If the two cities are not connected, return a large value
+        return int.MaxValue;
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int roadsRows = Convert.ToInt32(Console.ReadLine().Trim());
+        int roadsColumns = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<List<int>> roads = new List<List<int>>();
+
+        for (int i = 0; i < roadsRows; i++)
+        {
+            roads.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(roadsTemp => Convert.ToInt32(roadsTemp)).ToList());
+        }
+
+        int result = Result.numberOfWays(roads);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
---
+++ problem2.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+
+namespace HackerRank_problem_solving_intermadiate
+{
+
+public class AuthenticationSystem
+    {
+        public List<int> CheckAuthorizations(List<List<string>> events)
+        {
+            string password = null;
+            List<int> results = new List<int>();
+
+            long p = 131;
+            long M = (long)Math.Pow(10, 9) + 7;
+
+            foreach (List<string> ev in events)
+            {
+                string action = ev[0];
+                string input = ev[1];
+
+                if (action == "setPassword")
+                {
+                    password = input;
+                }
+                else if (action == "authorize")
+                {
+                    if (password == null)
+                    {
+                        results.Add(0); // No password set
+                    }
+                    else
+                    {
+                        bool authorized = false;
+                        int n = password.Length;
+
+                        // Check authorization with original password
+                        long originalHashValue = CalculateHash(password, p, M);
+                        if (originalHashValue.ToString() == input)
+                        {
+                            authorized = true;
+                            results.Add(authorized ? 1 : 0);
+                            continue;
+                        }
+
+                        // Check authorization with appended character
+                        foreach (char c in GetPossibleCharacters())
+                        {
+                            string appendedPassword = password + c;
+                            long appendedHashValue = CalculateHash(appendedPassword, p, M);
+                            if (appendedHashValue.ToString() == input)
+                            {
+                                authorized = true;
+                                break;
+                            }
+                        }
+
+                        results.Add(authorized ? 1 : 0);
+                    }
+                }
+            }
+
+            return results;
+        }
+
+        private long CalculateHash(string password, long p, long M)
+        {
+            long hashValue = 0;
+            int n = password.Length;
+
+            for (int i = 0; i < n; i++)
+            {
+                hashValue += (password[i] * ModPow(p, n - i - 1, M)) % M;
+                hashValue %= M;
+            }
+
+            return hashValue;
+        }
+
+        private long ModPow(long x, int y, long m)
+        {
+            if (y == 0)
+            {
+                return 1;
+            }
+            else if (y % 2 == 0)
+            {
+                long temp = ModPow(x, y / 2, m);
+                return (temp * temp) % m;
+            }
+            else
+            {
+                return (x * ModPow(x, y - 1, m)) % m;
+            }
+        }
+
+        private IEnumerable<char> GetPossibleCharacters()
+        {
+            // Define the possible characters that can be appended to the password
+            for (char c = ' '; c <= '~'; c++) // Range from space (' ') to tilde ('~')
+            {
+                yield return c;
+            }
+        }
+    }
+
+  
+ }
